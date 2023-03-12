916142713f35d4679f6ef6a66336fcdf20d8fc2d
Basheer Rajeh <muhammadbasheerrajeh@gmail.com>
2023-03-12 15:58:08 +0300

    fix style and add animation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+const btnContainer = document.getElementById('chat-btn-container');
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function () {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    btnContainer.classList.remove('show');
+  } else {
+    btnContainer.classList.add('show');
+  }
+  lastScrollTop = scrollTop;
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
+const icon = document.querySelector('.order-operation__icon');
+const observer = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    icon.classList.add('show');
+  }
+});
+observer.observe(icon);
